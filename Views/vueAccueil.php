ce54e71de522f091f72374affe21c5ef90f7a7c4
MorganMBA <mba@intechinfo.fr>
2020-06-22 10:24:22 +0200

    index

--- Views/vueAccueil.php
+++ Views/vueAccueil.php
@@ -2,7 +2,7 @@
 
 <?php ob_start(); ?>
     <?php 
-        echo "Bonjour ".$_SESSION['username'];
+        echo "Bonjour ".$_SESSION['mail'];
     ?>
 <?php $contenu = ob_get_clean(); ?>
 <?php require('templates/Tmp.php'); ?>
--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
+    require('./controllers/controller.php');
 
+    if(isset($_GET['action'])) {
+        switch($_GET['action']) {
+                case 'connexion':
+                    verifyConnect($_POST['mail'],$_POST['password']);
+                    break;
+        }            
+    } else {
+        require('./views/vueIndex.php');
+    }
 ?>
